0643df36a5ccb092e1a5f6b99df8a7d56847bf30
emedi <emedim.araneda@gmail.com>
2024-10-09 12:20:43 +0200

    update .env

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ app.delete('/tasks/:id', async (req, res) => {
 });
 
 //Démarrer le serveur
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}');
+    console.log(`Server running on port ${PORT}`);
 });
